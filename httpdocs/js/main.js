255136fbc6990b6d1fb796a4083df05f37a62351
George Brassey <george.brassey@themechanism.com>
2014-10-02 11:43:24 -0400

    homepage - quick fix for menu button

--- httpdocs/js/main.js
+++ httpdocs/js/main.js
@@ -11,7 +11,7 @@ jQuery(document).ready(function($) {
 		$("body").toggleClass("menu-active");
 		$(".main").on("touchstart click", function(e) {
 			var $target = $(e.target);
-			if ($target.attr("id") === "menu-trigger" || $target.parent().attr("id") === "menu-trigger") return;
+			if ($target.attr("id") === "menu-trigger" || $target.parents("#menu-trigger").length) return;
 			$(this).off();
 			$("body").removeClass("menu-active");
 		});
