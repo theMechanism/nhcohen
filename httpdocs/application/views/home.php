f2028a67605100ec1aa01350ff5b8650231ac3c5
George Brassey <george.brassey@themechanism.com>
2014-10-08 14:16:25 -0400

    homepage - ned requested changes

--- httpdocs/application/views/home.php
+++ httpdocs/application/views/home.php
@@ -11,7 +11,7 @@
 	    <div class="bg-container" style="background-image:url('<?php echo base_url('img/hero-banner.jpg'); ?>');"></div>
 	    <div class="container">
 	        <div class="hero-text">
-	            <h2>Perspective and insight,<br>for real estate investment<br>that stands the test of time</h2>
+	            <h2>Perspective and insight<br>for real estate investment<br>that stands the test of time</h2>
 	            <a class="hero-cta" href="#">Learn More</a>
 	        </div>
         </div>
