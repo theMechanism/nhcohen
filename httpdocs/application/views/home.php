748a62718d90a8617ac68755b0a428e293a9b459
George Brassey <george.brassey@themechanism.com>
2014-10-10 15:19:03 -0400

    better background images handling - moved to css/sass

--- httpdocs/application/views/home.php
+++ httpdocs/application/views/home.php
@@ -8,7 +8,7 @@
 		</div>
 	<?php endif ?>
 	<div class="hero-container">
-	    <div class="bg-container" style="background-image:url('<?php echo base_url('img/hero-banner.jpg'); ?>');"></div>
+	    <div class="bg-container"></div>
 	    <div class="container">
 	        <div class="hero-text">
 	            <h2>Perspective and insight<br>for real estate investment<br>that stands the test of time</h2>
