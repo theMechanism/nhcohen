d770afe1ad62b8814332a753873821b866269185
George Brassey <george.brassey@themechanism.com>
2014-11-21 12:36:28 -0500

    fix for ie8

--- httpdocs/application/views/footer.php
+++ httpdocs/application/views/footer.php
@@ -40,7 +40,9 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo base_url('js/vendor/jquery-1.11.1.min.js'); ?>"><\/script>')</script>
 
-        <script src="<?php echo base_url('js/vendor/fastclick.js'); ?>"></script>
+        <!--[if gt IE 8]>-->
+		<script src="<?php echo base_url('js/vendor/fastclick.js'); ?>"></script>
+		<!--<![endif]-->
         <script src="<?php echo base_url('js/main.js'); ?>"></script>
 
         <script>
