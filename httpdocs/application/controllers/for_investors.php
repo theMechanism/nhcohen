c757158ae63181a03beb6fee93059f09a4db0fac
Dave Fletcher <dave.fletcher@themechanism.com>
2015-02-10 11:26:30 -0500

    update form responders for both contact us and AIF form + update pdf for AIF
    Signed-off-by: Dave Fletcher <dave.fletcher@themechanism.com>

--- httpdocs/application/controllers/for_investors.php
+++ httpdocs/application/controllers/for_investors.php
@@ -313,7 +313,7 @@ class For_investors extends MY_Controller {
 
 		if ('success' == $action)
 		{
-			$this->data['success'] = 'Thank you for submitting your inquiry. We will respond shortly.</p><p>If you require immediate assistance, please contact Investor Services at <a href="tel:+12124986962">212.498.6962</a>';
+			$this->data['success'] = 'Thank you for submitting the Accredited Investor Suitability Form. We will be in contact with you shortly.</p><p>If you require immediate assistance, please contact Investor Services at <a href="tel:+12124986962">212.498.6962</a> or <a href="mailto:investorservices@nhcohenpartners.com">investorservices@nhcohenpartners.com</a>';
 		}
 
 		$this->load->view('template', $this->data);
--- httpdocs/application/views/contact_us-success.php
+++ httpdocs/application/views/contact_us-success.php
@@ -1,2 +1,2 @@
-<p>Thank you for submitting your inquiry. We will respond shortly.</p>
-<p>If you require immediate assistance, please contact Investor Services at <a href="tel:+12124986962">212.498.6962</a></p>
+<p>Thank you for contacting NHCohen Partners. We will respond to your inquiry shortly.</p>
+<p>If you require immediate assistance, please contact Investor Services at <a href="tel:+12124986962">212.498.6962</a> or <a href="mailto:investorservices@nhcohenpartners.com">investorservices@nhcohenpartners.com</a>. 
